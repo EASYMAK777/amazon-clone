79a801865b89a3b7237987225abaf9fb7b49a85a
JuanFlores <juanflz7777@gmail.com>
2022-08-31 12:31:27 -0700

    created product component, added product and product info divs

---
+++ src/Product.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      {/* Title */}
+      <div className="product_info">
+        <p>The lean startup</p>
+      </div>
+      {/* Price */}
+    </div>
+  );
+}
+
+export default Product;
