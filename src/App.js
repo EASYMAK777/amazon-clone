735b64ac863921b3e7e93864952a51180bc74dba
JuanFlores <juanflz7777@gmail.com>
2022-08-31 15:02:48 -0700

    final commit before stopping for the day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Link to="/checkout">
-          {/* Header and Checkout Component */}
-          <Header />
-        </Link>
-        <Link to="/">
-          {/* Header and Home Component */}
-          <Home />
-        </Link>
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+
+      <Home />
+    </div>
   );
 }
 export default App;
