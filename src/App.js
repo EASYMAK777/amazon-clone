a366e135a186f0774993a3f34d264599502e2167
JuanFlores <juanflz7777@gmail.com>
2022-08-31 14:34:10 -0700

    set up Router comp, and LInk comp using react-router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import "./App.css";
 
@@ -9,17 +9,16 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/">
-            {/* Header Component */}
-            <Header />
-            {/* Home Component */}
-            <Home />
-          </Route>
-        </Switch>
+        <Link to="/checkout">
+          {/* Header and Checkout Component */}
+          <Header />
+        </Link>
+        <Link to="/">
+          {/* Header and Home Component */}
+          <Home />
+        </Link>
       </div>
     </Router>
   );
 }
-
 export default App;
