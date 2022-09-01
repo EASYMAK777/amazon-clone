c59f76d66774c6b219a5ebb96df0af277e9041a3
JuanFlores <juanflz7777@gmail.com>
2022-09-01 01:31:06 -0700

    created ad for checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Checkout.css";
 
 function Checkout() {
-  return <div>Iam the checkout page</div>;
+  return (
+    <div className="checkout">
+      <div className="checkout_left">
+        <img
+          className="checkout_ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="advertisment"
+        ></img>
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
