6962b86f16cc776f147d088d046f78ce91153f4c
JuanFlores <juanflz7777@gmail.com>
2022-09-01 00:39:19 -0700

    commit before branch switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
-
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
