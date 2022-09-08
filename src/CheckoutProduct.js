ef8594239b19e285b00fedeeccf98f126d2b475a
JuanFlores <juanflz7777@gmail.com>
2022-09-08 09:04:47 -0700

    checkoutProduct component successfully totaling up shopping cart

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -3,7 +3,7 @@ import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
-  const [{ basket }, dispatch] = useStateValue;
+  const [{ basket }, dispatch] = useStateValue();
   // console.log("You've Removed an item >>>", basket);
   const removeFromBasket = () => {
     // Removes Item from basket
