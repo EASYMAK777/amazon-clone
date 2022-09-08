5f25d5081e7debe204a417b89c3e8695b5aa6c51
JuanFlores <juanflz7777@gmail.com>
2022-09-08 10:38:29 -0700

    configured firebase.js, function to create and store user authorization in firebase db

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAuG3uiK21AlCjO5yRe7ifMx5OJ_3bEiKY",
   authDomain: "react--clone-eb256.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:821419953601:web:67bab9e09214dfea38f340",
   measurementId: "G-JLCGBLKH7Y",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
