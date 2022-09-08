fdbf6b69e918ee6f64e570ba32025ee8a5801990
JuanFlores <juanflz7777@gmail.com>
2022-09-08 10:08:33 -0700

    useState to set email and password

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="login">
       <Link to="/">
